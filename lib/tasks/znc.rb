2b8c583d73d167dcdcc9589821b6cc0b4c233834
Keyvan Fatehi <keyvanfatehi@gmail.com>
2013-09-03 19:05:58 -0700

    Stubbed the autobuild -- #1

--- lib/tasks/znc.rb
+++ lib/tasks/znc.rb
@@ -4,13 +4,23 @@ namespace :znc do
     # Installs ZNC 1.0 if not installed
     # Resets the ZNC configuration to defaults for testing
     task :prepare do
-      `hash znc ; echo $?`.strip == 0
+      if `hash znc > /dev/null 2>&1; echo $?`.strip.to_i == 0
+        # Yes its installed
+        # replace the config file
+        puts "Replace the config file"
+      else
+        # install it
+        # are we in mac?
+        # Yes? 
+        # Then brew install zsh if brew is installed
+        raise "ZNC is not installed, cannot prepare for test"
+      end
       # First: Is it installed ?
       #        Is it v1.0 ?
+      #
       # If not, install v1.0
       # If yes -- reset the configuration
       # Reuse this code in spec_helper
-      
     end
   end
 end
